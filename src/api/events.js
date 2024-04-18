92e087886f2309378ca720991d0c93981f08c432
Jerome <j.moreau4884@hotmail.com>
2024-04-18 12:30:11 +0200

    CardEvent, Event

--- src/api/events.js
+++ src/api/events.js
@@ -32,7 +32,20 @@ export const createEvent = async (
         }),
       });
       const data = await response.json();
+      console.log('data create event => ', data);
       return data;
+    } catch (error) {
+      console.error("Error during creation:", error.message);
+    }
+  };
+
+  export const displayEvent = async (token) => {
+    try {
+      const response = await fetch(`http://localhost:3000/events/displayEvent/${token}`, {
+      });
+      const data = await response.json();
+      console.log('data display event => ', data.event);
+      return data.event;
     } catch (error) {
       console.error("Error during creation:", error.message);
     }
