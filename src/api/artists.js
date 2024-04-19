39210bb8a205dbbecbdbc25a2407ec332a93496e
Oscar Chaumuzeau <oscar.chaumuzeau@gmail.com>
2024-04-19 17:13:14 +0200

    props page display v1

--- src/api/artists.js
+++ src/api/artists.js
@@ -1,6 +1,4 @@
-
-
-// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour 
+// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour
 // l'inscription d'un nouvel utilisateur
 export const signUpArtist = async (email, password) => {
   try {
@@ -18,7 +16,7 @@ export const signUpArtist = async (email, password) => {
   }
 };
 
-// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour 
+// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour
 // la connnexion d'un utilisateur
 export const signInArtist = async (email, password) => {
   try {
@@ -36,7 +34,7 @@ export const signInArtist = async (email, password) => {
   }
 };
 
-// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour 
+// Fonction de liaison vers le backend et de controle des données de l'utilisateur pour
 // la mise à jour d'un profil utilisateur.
 export const updateArtist = async (
   token,
@@ -54,26 +52,29 @@ export const updateArtist = async (
   spotify
 ) => {
   try {
-    const response = await fetch(`http://localhost:3000/artists/createProfile/${token}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        type: type,
-        description: description,
-        members: members,
-        picture: picture,
-        genres: genres,
-        medias: medias,
-        youtube: youtube,
-        soundcloud: soundcloud,
-        facebook: facebook,
-        deezer: deezer,
-        spotify: spotify,
-      }),
-    });
+    const response = await fetch(
+      `http://localhost:3000/artists/createProfile/${token}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          type: type,
+          description: description,
+          members: members,
+          picture: picture,
+          genres: genres,
+          medias: medias,
+          youtube: youtube,
+          soundcloud: soundcloud,
+          facebook: facebook,
+          deezer: deezer,
+          spotify: spotify,
+        }),
+      }
+    );
     const data = await response.json();
     return data;
   } catch (error) {
@@ -83,7 +84,7 @@ export const updateArtist = async (
 
 // Fonction de liaison vers le backend et de controle des données pour récupérer les données sur un artiste.
 export const getArtist = async (token) => {
-  try{
+  try {
     const response = await fetch(`http://localhost:3000/artists/${token}`, {
       method: "GET",
       headers: {
@@ -91,28 +92,42 @@ export const getArtist = async (token) => {
       },
     });
     const data = await response.json();
-    return data.artist
-  }catch(error){
-    console.error("Error retrieving artist infos:", error.message)
+    return data.artist;
+  } catch (error) {
+    console.error("Error retrieving artist infos:", error.message);
   }
-}
-
+};
 
 export const getArtists = async () => {
-  try{
+  try {
     const response = await fetch(`http://localhost:3000/artists`, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
     });
-    let data = await response.json()
-    if(data){
-      return data
-    }else{
-      return data = {message: "error"}
+    let data = await response.json();
+    if (data) {
+      return data;
+    } else {
+      return (data = { message: "error" });
     }
-  }catch(error){
-    console.error("Error retrieving artists:", error.message)
+  } catch (error) {
+    console.error("Error retrieving artists:", error.message);
   }
-}
+};
+
+export const getArtistById = async (id) => {
+  try {
+    const response =fetch(`http://localhost:3000/artists/id/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching an artist: ", error.message);
+  }
+};
