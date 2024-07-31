d52fd939aafa4b9f6a874e6a76656968ee55a235
Jerome <j.moreau4884@hotmail.com>
2024-07-31 15:05:50 +0200

    first commit

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,10 +1,14 @@
-
+// fonction generique pour l'upload de fichier
 export const uploadFile = async (event) => {
+    // Récupération du fichier
     const file = event.target.files[0];
     try {
+        // Création d'un objet FormData pour envoyer les données du fichier
       const formData = new FormData();
+        // Ajout du fichier à l'objet FormData sous le nom 'image'
        formData.append('image', file);
-      const response = await fetch('https://bartist-backend.vercel.app/uploadFile', {
+       
+      const response = await fetch('http://localhost:3000/uploadFile', {
         method: 'POST',
         body: formData,
       })
